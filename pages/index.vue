cc2049152bf9af407ffb7a73c789f115a77ddfe1
Taku Ogawa <ogawtaku@gmail.com>
2022-05-11 01:24:26 +0900

    jsonplaceholderから情報を取得する

--- pages/index.vue
+++ pages/index.vue
@@ -4,11 +4,24 @@
     <hr />
     <router-link to="/price">Price Page</router-link>
     <router-link to="/mypage">My Page</router-link>
+
+    <div>
+      {{ users[0].id }}, {{ users[0].name }}
+    </div>
+
   </section>
 </template>
 
 <script>
+const axios = require("axios");
+let url = "https://jsonplaceholder.typicode.com/users";
+
 export default {
+  asyncData({ params }) {
+    return axios.get(url).then((res) => {
+      return { users: res.data };
+    });
+  },
   name: "IndexPage",
   data: function () {
     return {
