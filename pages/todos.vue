b6515c75c078f526ba13814da4f245aadb664ed1
Taku Ogawa <ogawtaku@gmail.com>
2022-05-12 00:03:30 +0900

    完了/未完了の更新処理を追加

--- pages/todos.vue
+++ pages/todos.vue
@@ -1,47 +1,58 @@
 <template>
-    <div>
-        <h1>TODO app</h1>
-        <div class="form">
-            <!-- addボタンを押された時、リロードを無効にする -->
-            <form v-on:submit.prevent="add">
-                <input v-model="name">
-                <button>Add</button>
-            </form>
-        </div>
-
-        <!-- {{ todos }} -->
-        <ul>
-            <li v-for="todo in todos" :key="todo.id">
-                {{ todo.done }} {{ todo.name }} {{ todo.created }}
-            </li>
-        </ul>
-
+  <div>
+    <h1>TODO app</h1>
+    <div class="form">
+      <!-- addボタンを押された時、リロードを無効にする -->
+      <form v-on:submit.prevent="add">
+        <input v-model="name" />
+        <button>Add</button>
+      </form>
     </div>
+
+    <!-- {{ todos }} -->
+    <ul>
+      <li v-for="todo in todos" :key="todo.id">
+        <input
+          type="checkbox"
+          v-bind:checked="todo.done"
+          @change="toggle(todo)"
+        />
+        {{ todo.name }} {{ todo.created }}
+        <button v-on:click="remove(todo.id)">X</button>
+      </li>
+    </ul>
+  </div>
 </template>
 
 <script>
 export default {
-    // 一時的に格納するデータ
-    data: function() {
-        return {
-            name: '',
-            done: false
-        }
+  // 一時的に格納するデータ
+  data: function () {
+    return {
+      name: "",
+      done: false,
+    };
+  },
+  created: function () {
+    this.$store.dispatch("todos/init");
+  },
+  methods: {
+    // firebaseに追加する
+    add() {
+      this.$store.dispatch("todos/add", this.name);
+      this.name = "";
     },
-    created: function() {
-        this.$store.dispatch('todos/init')
+    remove(id) {
+      this.$store.dispatch("todos/remove", id);
     },
-    methods: {
-        // firebaseに追加する
-        add() {
-            this.$store.dispatch('todos/add', this.name)
-            this.name = ''
-        }
+    toggle(todo) {
+      this.$store.dispatch("todos/toggle", todo);
     },
-    computed: {
-        todos() {
-            return this.$store.state.todos.todos
-        }
-    }
-}
+  },
+  computed: {
+    todos() {
+      return this.$store.state.todos.todos;
+    },
+  },
+};
 </script>
