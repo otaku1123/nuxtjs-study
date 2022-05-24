01612f29759b181e123813670eda2964decff5ae
Taku Ogawa <ogawtaku@gmail.com>
2022-05-24 19:04:22 +0900

    コードフォーマット

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,15 +8,32 @@ export default {
         htmlAttrs: {
             lang: 'en'
         },
-        meta: [
-            { charset: 'utf-8' },
-            { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-            { hid: 'description', name: 'description', content: '' },
-            { name: 'format-detection', content: 'telephone=no' }
+        meta: [{
+                charset: 'utf-8'
+            },
+            {
+                name: 'viewport',
+                content: 'width=device-width, initial-scale=1'
+            },
+            {
+                hid: 'description',
+                name: 'description',
+                content: ''
+            },
+            {
+                name: 'format-detection',
+                content: 'telephone=no'
+            }
         ],
-        link: [
-            { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-            { rel: 'stylesheet', href: 'https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap' }
+        link: [{
+                rel: 'icon',
+                type: 'image/x-icon',
+                href: '/favicon.ico'
+            },
+            {
+                rel: 'stylesheet',
+                href: 'https://fonts.googleapis.com/css2?family=Permanent+Marker&display=swap'
+            }
         ]
     },
 
